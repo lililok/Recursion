b527827d8e0ad8757340ebbd820a1c2435c6b094
lililok <eshanu.sofia@yandex.ru>
2024-06-13 09:28:35 +0300

    sketches

---
+++ linked lists/link.js
@@ -0,0 +1,52 @@
+class LinkedList {
+    constructor() {
+
+    }
+
+    append(value) {
+
+    }
+
+    prepend(value) {
+
+    }
+
+    size() {
+
+    }
+
+    head() {
+
+    }
+
+    tail() {
+
+    }
+
+    at(index) {
+
+    }
+
+    pop() {
+
+    }
+
+    contains(value) {
+
+    }
+
+    find(value) {
+
+    }
+
+    toString() {
+
+    }
+}
+
+class Node {
+    constructor() {
+        this.value = null
+        this.nextNode = null
+    }
+}
