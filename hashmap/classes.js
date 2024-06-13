bfab12506e73f82da619ab3d6863189386aa5dc3
lililok <eshanu.sofia@yandex.ru>
2024-06-13 11:56:15 +0300

    hashmap sketches

---
+++ hashmap/classes.js
@@ -0,0 +1,45 @@
+export class Hashmap {
+    constructor() {
+
+    }
+
+    hash(key) {
+
+    }
+
+    set(key, value) {
+
+    }
+
+    get(key) {
+
+    }
+
+    has(key) {
+
+    }
+
+    remove(key) {
+
+    }
+
+    length() {
+
+    }
+
+    clear() {
+
+    }
+
+    keys() {
+
+    }
+
+    values() {
+
+    }
+
+    entries() {
+        
+    }
+}
